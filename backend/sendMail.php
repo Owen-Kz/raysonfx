bea5c5bd656859db68743c2faee036c6771eb43b
Benson Michael Owen <lordkz19@gmail.com>
2024-03-21 21:27:24 +0100

    Update sendMail.php

--- backend/sendMail.php
+++ backend/sendMail.php
@@ -23,7 +23,7 @@ if($RecipientEmail){
 $sendgrid = new \SendGrid($api);
 try {
         $email = new \SendGrid\Mail\Mail();
-        $email->setFrom($senderEmail, "AlphaForex Lyfe");
+        $email->setFrom(trim($senderEmail), "AlphaForex Lyfe");
         $email->setSubject($subject);
         $email->addTo($RecipientEmail, $RecipientName);
         $email->addContent(
