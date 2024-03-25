27257c013032e706c7fd92f241077f9054bbc656
Benson Michael Owen <lordkz19@gmail.com>
2024-03-25 14:09:38 +0100

    Update db.php

--- backend/db.php
+++ backend/db.php
@@ -14,7 +14,10 @@ $con = mysqli_connect($servername, $username, $password, $db);
 
 // Check connection
 if (!$con) {
+    $response = array('status' => 'error', 'error' => mysqli_connect_error());
+    echo json_encode($response);
     die("Connection failed: " . mysqli_connect_error());
+
 }
 
 
