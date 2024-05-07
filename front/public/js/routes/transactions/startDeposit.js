8ee566f94e5183ca5fd35f031eefb50857614c72
Benson Michael Owen <lordkz19@gmail.com>
2024-05-07 19:04:28 +0100

    Wallet Address Added to Deposit Page

--- front/public/js/routes/transactions/startDeposit.js
+++ front/public/js/routes/transactions/startDeposit.js
@@ -1,18 +1,32 @@
 import { parentDirectoryName } from "../constants.js";
+import { GetSiteData } from "../foreman/siteData.js";
 import { GetCookie, SetCookies, hoursToKeep } from "../setCookies.js"
 
 const DepositForm = document.getElementById("depositForm")
 const userID = GetCookie("u_id")
 
 const Amount = document.getElementById("amount");
+const gateway = document.getElementById("gateway")
+
+const SiteData = await GetSiteData()
+
+let gatewayText 
+if(gateway.value === "1000"){
+    gatewayText = "BTC"
+}else{
+    gatewayText = "ETH"
+}
+
 
 DepositForm.addEventListener("submit", function(e){
     e.preventDefault()
     const TransacttionDetails = {
         user_id : userID,
         amount: Amount.value,
+        gateway:gatewayText,
         type :"depositWalletCredit",
     }
     SetCookies("t_rd", JSON.stringify(TransacttionDetails), hoursToKeep)
+
     window.location.href = `${parentDirectoryName}/user/deposit/manual.html`
 })
--- front/public/js/routes/transactions/uploadTransactionReceipt.js
+++ front/public/js/routes/transactions/uploadTransactionReceipt.js
@@ -1,10 +1,44 @@
 import { ENDPOINT, parentDirectoryName } from "../constants.js"
+import { GetSiteData } from "../foreman/siteData.js"
 import { DeleteCookie, GetCookie } from "../setCookies.js"
 
-
+console.log("manual")
 const FileUploadForm = document.getElementById("file_uploadForm")
 const TransactionDetails = GetCookie('t_rd')
+const gatewayContent = document.getElementById("gatewayContent")
+
+const header = document.getElementById("walletHeader")
+
 if(TransactionDetails){
+const GateWay = JSON.parse(TransactionDetails).gateway
+const Details = JSON.parse(TransactionDetails)
+
+const SiteData = await GetSiteData()
+
+if(GateWay === "ETH"){
+    header.innerHTML = `<i class="las la-wallet"></i> Crypto Payment (ETHEREUM &amp; USDT ERC20) Payment`
+    const ToETH = Details.amount * 1
+
+    gatewayContent.innerHTML = `<p class="text-center mt-2">You have requested 
+        <b class="text--success">${new Number(Details.amount).toFixed(2)} USD</b> ,
+        Please pay <b class="text--success">${ToETH} ETH-USDT </b> for successful payment                     </p>
+        <div class="my-4">
+            <p><span style="font-family: Montserrat, sans-serif;"><font color="#ffffff">Please Kindly Make Payment To This ETHEREUM &amp;&nbsp; USDT ERC20 Wallet Address:&nbsp;</font></span><font color="#ffffff" face="Montserrat, sans-serif">${SiteData.eth}</font><br></p>
+        </div>`;
+}else{
+    header.innerHTML = `<i class="las la-wallet"></i> Crypto Payment (BTC) Payment`
+    const ToBTC = Details.amount * 0.000016
+
+     gatewayContent.innerHTML = `
+    <p class="text-center mt-2">You have requested 
+    <b class="text--success">${new Number(Details.amount).toFixed(2)} USD</b> ,
+    Please pay <b class="text--success">${ToBTC} BTC </b> for successful payment
+    </p>
+
+        <div class="my-4">
+            <p><span style="font-family: Montserrat, sans-serif;"><font color="#ffffff">Please Kindly Make Payment To This BTC Wallet Address:&nbsp;</font></span><font color="#ffffff" face="Montserrat, sans-serif">${SiteData.btc}</font><br></p>
+        </div>`
+}
 
 FileUploadForm.addEventListener("submit", function(e){
     e.preventDefault();
